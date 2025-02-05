33c7dfbfd20a6b678d1c8549f8ece16cb710a96c
rohitsharma342 <rohitkumar.brainbox@gmail.com>
2025-02-05 21:18:02 +0530

    sd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT || 3000, // Use Render's assigned port
+    host: '0.0.0.0', // Ensures it's accessible
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  }
+});
