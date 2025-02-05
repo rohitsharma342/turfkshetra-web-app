6e2d5e661a2badb45d7a2884edae84b0a140a7c3
rohitsharma342 <rohitkumar.brainbox@gmail.com>
2025-02-05 21:22:24 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: process.env.PORT || 3000, // Use Render's assigned port
-    host: '0.0.0.0', // Ensures it's accessible
+    port: process.env.PORT || 3000, 
+    host: '0.0.0.0', 
+    strictPort: true, // Ensures Vite uses the exact port
+    allowedHosts: ["turfkshetra-web-app.onrender.com"], // Allow Render host
   },
   preview: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0',
+    allowedHosts: ["turfkshetra-web-app.onrender.com"],
   }
 });
